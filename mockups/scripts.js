280b76dfcac7d64a40981e4105b376f78a09c535
Christine Do <christinebdo@gmail.com>
2015-09-21 01:33:18 -0400

    dialog

--- mockups/scripts.js
+++ mockups/scripts.js
@@ -1,3 +1,12 @@
-$( ".pplcol" ).click(function() {
-  $( ".finishedtasks" ).append("<span>★</span>");
+$( "#addChore" ).dialog({
+	autoOpen: false,
+	height: 500,
+	width: 500,
+	modal: true
+	});
+$( ".name" ).click(function() {
+	$( "#addChore" ).dialog( "open" );
+});
+$("#selectChore").click(function() {
+	$(".starcount").append('<img class="star" src="star.png">');
 });
