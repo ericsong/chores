a757bcf3dd6d1f9b0ecda1e0bd83e847dc6ad8ee
ericsong <regonics@gmail.com>
2015-09-28 23:14:00 -0400

    serving mockup page with django

---
+++ chores/web/static/scripts.js
@@ -0,0 +1,12 @@
+$( "#addChore" ).dialog({
+	autoOpen: false,
+	height: 500,
+	width: 500,
+	modal: true
+	});
+$( ".name" ).click(function() {
+	$( "#addChore" ).dialog( "open" );
+});
+$("#selectChore").click(function() {
+	$(".starcount").append('<img class="star" src="/static/star.png">');
+});
